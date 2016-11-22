033966ef0207b055c5951c91dd54c1f3758af26e
Sankar <sankar.nathanresearch#gmail.com>
2016-11-22 12:15:29 +0530

    empty

--- RLMS/src/r_lms_qa/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
- <footer>
-          <div class="pull-right">
-            Remodelers <a href="https://nathanresearch.com">Nathan Research Inc</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-	
-		
